6bf70e1604861b94ab738dd9b449e307f39ee7a5
danpaul <pt2323@gmail.com>
2015-05-05 22:36:37 -0400

    cleanup

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.get('/', function(req, res){ res.sendFile('./public/index.html'); });
 
 *******************************************************************************/
 
-var server = app.listen(3000, function () {
+var server = app.listen(config.port, function () {
     var host = server.address().address
     var port = server.address().port
     console.log("Example app listening at http://%s:%s", host, port)
